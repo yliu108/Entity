a828b647c9a632f411a68ddd4d6d631658dcda59
yliu108 <43041131+yliu108@users.noreply.github.com>
2020-07-30 16:46:57 -0600

    databaseFirst fix

---
+++ DatabaseFirst/DatabaseFirst/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+namespace DatabaseFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbContext = new VidzyDbContext();
+            dbContext.AddVideo("testing", DateTime.Now,"Horror",(byte)Classification.Platinum);
+            dbContext.AddVideo("testing2", DateTime.Now, "Comedy", (byte)Classification.Gold);
+            Console.Read();
+        }
+    }
+}
