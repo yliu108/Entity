7164a3c98a31835dd911c0e60bb5bab9e40c704d
yliu108 <43041131+yliu108@users.noreply.github.com>
2020-07-29 18:59:40 -0600

    codefirst

---
+++ CodeFirst/CodeFirst/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CodeFirst
+{
+    class Program
+    {
+        public class Video
+        {
+            public int Id {set; get;  }
+            public string Name { set; get; }
+            public DateTime ReleaseDate { set; get; }
+        }
+        public class Genre
+        {
+            public byte Id { set; get; }
+            public string Name { set; get; }
+        }
+
+        public class VidzyContext:DbContext
+        {
+            public DbSet<Video> Videos { set; get; }
+            public DbSet<Genre> Genres { set; get; }
+            public VidzyContext():base("name=Connection")
+            {
+
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
